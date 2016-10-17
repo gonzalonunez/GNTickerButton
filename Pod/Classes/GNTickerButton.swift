842db9e5e11211289d6c183943bb4df153f1a6cd
gonzalonunez <gonzi@tcpmiami.com>
2016-10-17 15:07:44 -0400

    Updated to Swift 3

--- Pod/Classes/GNTickerButton.swift
+++ Pod/Classes/GNTickerButton.swift
@@ -8,272 +8,270 @@
 
 import UIKit
 
-@objc public protocol GNTickerButtonRotationDelegate {
-    func tickerButtonTickerRotated(tickerButton button:GNTickerButton)
+public protocol GNTickerButtonRotationDelegate: class {
+  func tickerButtonTickerRotated(tickerButton button:GNTickerButton)
 }
 
-@IBDesignable public class GNTickerButton : UIButton {
+@IBDesignable open class GNTickerButton : UIButton {
+  
+  static fileprivate let kInnerRingLineWidth:CGFloat = 1
+  static fileprivate let kOuterRingLineWidth:CGFloat = 4
+  static fileprivate let kOutterInnerRingSpacing:CGFloat = 6
+  static fileprivate let kTearDropRadius:CGFloat = 5
+  
+  static fileprivate let kTickerRotationAnimationKey = "transform.rotation"
+  static fileprivate let kRingProgressAnimationKey = "strokeEnd"
+  
+  static fileprivate let kRingProgressAnimationDuration = 0.15
+  
+  @IBInspectable open var fillColor = UIColor(red: 251/255, green: 77/255, blue: 31/255, alpha: 1) {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  @IBInspectable open var ringColor = UIColor.white {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  @IBInspectable open var tickerColor  = UIColor.white {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  open var shouldShowRingProgress = true {
+    didSet {
+      ringLayer.isHidden = !shouldShowRingProgress
+    }
+  }
+  
+  fileprivate var isPressed : Bool = false {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  fileprivate(set) var tickerIsSpinning = false
+  
+  fileprivate var tickerLayer = CAShapeLayer()
+  fileprivate var ringLayer = CAShapeLayer()
+  
+  fileprivate var desiredRotations:Int?
+  
+  weak var delegate : GNTickerButtonRotationDelegate?
+  
+  //MARK: - Initiliazation
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    addTargets()
+  }
+  
+  override public init(frame: CGRect) {
+    super.init(frame: frame)
+    addTargets()
+  }
+  
+  //MARK: - Set Up
+  
+  fileprivate func setUpTicker() {
+    tickerLayer.removeFromSuperlayer()
     
-    static private let kInnerRingLineWidth:CGFloat = 1
-    static private let kOuterRingLineWidth:CGFloat = 4
-    static private let kOutterInnerRingSpacing:CGFloat = 6
-    static private let kTearDropRadius:CGFloat = 5
+    let centerX = bounds.midX
+    let centerY = bounds.midY
     
-    static private let kTickerRotationAnimationKey = "transform.rotation"
-    static private let kRingProgressAnimationKey = "strokeEnd"
+    let outerRadius = outerRadiusInRect(bounds)
+    let innerRadius = outerRadius - GNTickerButton.kOutterInnerRingSpacing
     
-    static private let kRingProgressAnimationDuration = 0.15
+    let path = CGMutablePath()
+    let padding = 8 as CGFloat
     
-    @IBInspectable public var fillColor = UIColor(red: 251/255, green: 77/255, blue: 31/255, alpha: 1) {
-        didSet {
-            setNeedsDisplay()
+    path.addArc(center: CGPoint(x: centerX, y: centerY), radius: GNTickerButton.kTearDropRadius, startAngle: 2 * CGFloat.pi, endAngle: CGFloat.pi, clockwise: false)
+    path.addLine(to: CGPoint(x: centerX, y: centerY  - innerRadius + padding))
+    path.addLine(to: CGPoint(x: centerX + GNTickerButton.kTearDropRadius, y: centerY))
+    
+    let tearDropHeight = innerRadius - padding
+    
+    let boundingBox = path.boundingBox
+    let height = boundingBox.height
+    let anchorY = 1 - (height - tearDropHeight)/height
+    
+    tickerLayer.anchorPoint = CGPoint(x: 0.5, y: anchorY)
+    tickerLayer.position = CGPoint(x: layer.bounds.midX, y: layer.bounds.midY)
+    tickerLayer.bounds = boundingBox
+    tickerLayer.path = path
+    tickerLayer.fillColor = tickerColor.cgColor
+    tickerLayer.strokeColor = tickerColor.cgColor
+    
+    layer.addSublayer(tickerLayer)
+  }
+  
+  fileprivate func setUpRing() {
+    ringLayer.removeFromSuperlayer()
+    
+    let rect = layer.bounds
+    let outerRadius = outerRadiusInRect(rect)
+    let centerX = rect.midX
+    let centerY = rect.midY
+    
+    let ringPath = CGMutablePath()
+    ringPath.addArc(center: CGPoint(x: centerX, y: centerY), radius: outerRadius, startAngle: -CGFloat.pi/2, endAngle: 3 * CGFloat.pi/2, clockwise: false)
+    
+    ringLayer.path = ringPath
+    ringLayer.position = CGPoint(x: layer.bounds.midX, y: layer.bounds.midY)
+    ringLayer.bounds = ringPath.boundingBox
+    ringLayer.fillColor = UIColor.clear.cgColor
+    ringLayer.strokeColor = ringColor.cgColor
+    
+    ringLayer.lineWidth = GNTickerButton.kOuterRingLineWidth
+    ringLayer.strokeEnd = 0
+    
+    layer.addSublayer(ringLayer)
+  }
+  
+  fileprivate func addTargets() {
+    addTarget(self, action: #selector(GNTickerButton.touchDown), for: .touchDown)
+    addTarget(self, action: #selector(GNTickerButton.touchUpInside), for: .touchUpInside)
+    addTarget(self, action: #selector(GNTickerButton.touchUpOutside), for: .touchUpOutside)
+  }
+  
+  @objc fileprivate func touchDown() {
+    isPressed = true
+  }
+  
+  @objc fileprivate func touchUpInside() {
+    isPressed = false
+  }
+  
+  @objc fileprivate func touchUpOutside() {
+    isPressed = false
+  }
+  
+  //MARK: Public
+  
+  open func rotateTickerWithDuration(_ duration:CFTimeInterval, rotations repeatCount:Int = 1, rotationBlock: ((Void) -> Void)?) {
+    _rotateTickerWithDuration(duration, rotations: repeatCount, shouldSetDesiredRotationCount: desiredRotations == nil, rotationBlock: rotationBlock)
+  }
+  
+  open func stopRotatingTicker() {
+    tickerLayer.removeAnimation(forKey: GNTickerButton.kTickerRotationAnimationKey)
+  }
+  
+  open func clearRingProgress() {
+    CATransaction.begin()
+    CATransaction.setCompletionBlock() {
+      CATransaction.begin()
+      self.ringLayer.strokeStart = 0
+      self.ringLayer.strokeEnd = 0
+      CATransaction.commit()
+    }
+    ringLayer.strokeStart = 1
+    CATransaction.commit()
+  }
+  
+  //MARK: Private
+  
+  fileprivate func _rotateTickerWithDuration(_ duration:CFTimeInterval, rotations repeatCount:Int = 1, shouldSetDesiredRotationCount:Bool = true, rotationBlock: ((Void) -> Void)?) {
+    tickerIsSpinning = true
+    
+    if (shouldSetDesiredRotationCount) {
+      desiredRotations = repeatCount
+    }
+    
+    let rotationAnimation = CABasicAnimation(keyPath: GNTickerButton.kTickerRotationAnimationKey)
+    rotationAnimation.duration = duration
+    rotationAnimation.fromValue = 0
+    rotationAnimation.toValue = 2*M_PI
+    rotationAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    
+    CATransaction.begin()
+    CATransaction.setCompletionBlock() {
+      DispatchQueue.main.async {
+        self.updateRingProgress(repeatCount, animated: true)
+        if (rotationBlock != nil) {
+          rotationBlock!()
+        } else {
+          self.delegate?.tickerButtonTickerRotated(tickerButton: self)
         }
-    }
-    
-    @IBInspectable public var ringColor = UIColor.whiteColor() {
-        didSet {
-            setNeedsDisplay()
+        if (repeatCount > 0) {
+          self.rotateTickerWithDuration(duration, rotations: repeatCount - 1, rotationBlock: rotationBlock)
+        } else {
+          self.desiredRotations = nil
+          self.tickerIsSpinning = false
         }
+      }
     }
     
-    @IBInspectable public var tickerColor  = UIColor.whiteColor() {
-        didSet {
-            setNeedsDisplay()
-        }
+    tickerLayer.add(rotationAnimation, forKey: GNTickerButton.kTickerRotationAnimationKey)
+    CATransaction.commit()
+  }
+  
+  fileprivate func updateRingProgress(_ rotationsLeft:Int, animated:Bool) {
+    var strokeEnd = 0 as CGFloat
+    if (desiredRotations != nil) {
+      strokeEnd = CGFloat((desiredRotations! - rotationsLeft)) / CGFloat(desiredRotations!)
     }
     
-    public var shouldShowRingProgress = true {
-        didSet {
-            ringLayer.hidden = !shouldShowRingProgress
-        }
+    let fillAnimation = CABasicAnimation(keyPath: GNTickerButton.kRingProgressAnimationKey)
+    fillAnimation.duration = animated ? GNTickerButton.kRingProgressAnimationDuration : 0
+    fillAnimation.fromValue = ringLayer.strokeEnd
+    fillAnimation.toValue = strokeEnd
+    fillAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    
+    ringLayer.strokeEnd = strokeEnd
+    
+    CATransaction.begin()
+    ringLayer.add(fillAnimation, forKey: GNTickerButton.kRingProgressAnimationKey)
+    CATransaction.commit()
+  }
+  
+  //MARK: - Drawing
+  
+  override open func draw(_ rect: CGRect) {
+    super.draw(rect)
+    
+    func addCircleInContext(_ context:CGContext, centerX:CGFloat, centerY:CGFloat, radius:CGFloat) {
+      context.addArc(center: CGPoint(x: centerX, y: centerY), radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
     }
     
-    private var isPressed : Bool = false {
-        didSet {
-            setNeedsDisplay()
-        }
+    guard let context = UIGraphicsGetCurrentContext() else {
+      return
     }
     
-    private(set) var tickerIsSpinning = false
+    let color = isPressed ? fillColor.withAlphaComponent(0.5) : fillColor
+    context.setFillColor(color.cgColor)
     
-    private var tickerLayer = CAShapeLayer()
-    private var ringLayer = CAShapeLayer()
+    context.setStrokeColor(ringColor.cgColor)
     
-    private var desiredRotations:Int?
+    let outerRadius = outerRadiusInRect(rect)
+    let innerRadius = outerRadius - GNTickerButton.kOutterInnerRingSpacing
     
-    weak var delegate : GNTickerButtonRotationDelegate?
+    let centerX = rect.midX
+    let centerY = rect.midY
     
-    //MARK: - Initiliazation
+    // Inner Circle
+    addCircleInContext(context, centerX: centerX, centerY: centerY, radius: innerRadius)
+    context.fillPath()
     
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        addTargets()
-    }
-    
-    override public init(frame: CGRect) {
-        super.init(frame: frame)
-        addTargets()
-    }
-    
-    //MARK: - Set Up
-    
-    private func setUpTicker() {
-        tickerLayer.removeFromSuperlayer()
-        
-        let centerX = CGRectGetMidX(bounds)
-        let centerY = CGRectGetMidY(bounds)
-        
-        let outerRadius = outerRadiusInRect(bounds)
-        let innerRadius = outerRadius - GNTickerButton.kOutterInnerRingSpacing
-        
-        let path = CGPathCreateMutable()
-        let padding = 8 as CGFloat
-        
-        CGPathAddArc(path, nil, centerX, centerY, GNTickerButton.kTearDropRadius,  CGFloat(2*M_PI),  CGFloat(M_PI), false)
-        CGPathAddLineToPoint(path, nil, centerX, centerY  - innerRadius + padding)
-        CGPathAddLineToPoint(path, nil, centerX + GNTickerButton.kTearDropRadius, centerY)
-        
-        let tearDropHeight = innerRadius - padding
-        
-        let boundingBox = CGPathGetBoundingBox(path)
-        let height = CGRectGetHeight(boundingBox)
-        let anchorY = 1 - (height - tearDropHeight)/height
-        
-        tickerLayer.anchorPoint = CGPoint(x: 0.5, y: anchorY)
-        tickerLayer.position = CGPoint(x: CGRectGetMidX(layer.bounds), y: CGRectGetMidY(layer.bounds))
-        tickerLayer.bounds = boundingBox
-        tickerLayer.path = path
-        tickerLayer.fillColor = tickerColor.CGColor
-        tickerLayer.strokeColor = tickerColor.CGColor
-        
-        layer.addSublayer(tickerLayer)
-    }
-    
-    private func setUpRing() {
-        ringLayer.removeFromSuperlayer()
-        
-        let rect = layer.bounds
-        let outerRadius = outerRadiusInRect(rect)
-        let centerX = CGRectGetMidX(rect)
-        let centerY = CGRectGetMidY(rect)
-        
-        let ringPath = CGPathCreateMutable()
-        CGPathAddArc(ringPath, nil, centerX, centerY, outerRadius, CGFloat(-M_PI_2), CGFloat(M_PI_2*3), false)
-        
-        ringLayer.path = ringPath
-        ringLayer.position = CGPoint(x: CGRectGetMidX(layer.bounds), y: CGRectGetMidY(layer.bounds))
-        ringLayer.bounds = CGPathGetBoundingBox(ringPath)
-        ringLayer.fillColor = UIColor.clearColor().CGColor
-        ringLayer.strokeColor = ringColor.CGColor
-        
-        ringLayer.lineWidth = GNTickerButton.kOuterRingLineWidth
-        ringLayer.strokeEnd = 0
-        
-        layer.addSublayer(ringLayer)
-    }
-    
-    private func addTargets() {
-        addTarget(self, action: "touchDown", forControlEvents: .TouchDown)
-        addTarget(self, action: "touchUpInside", forControlEvents: .TouchUpInside)
-        addTarget(self, action: "touchUpOutside", forControlEvents: .TouchUpOutside)
-    }
-    
-    @objc private func touchDown() {
-        isPressed = true
-    }
-    
-    @objc private func touchUpInside() {
-        isPressed = false
-    }
-    
-    @objc private func touchUpOutside() {
-        isPressed = false
-    }
-    
-    //MARK: Public
-    
-    public func rotateTickerWithDuration(duration:CFTimeInterval, rotations repeatCount:Int = 1, rotationBlock: (Void -> Void)?) {
-        _rotateTickerWithDuration(duration, rotations: repeatCount, shouldSetDesiredRotationCount: desiredRotations == nil, rotationBlock: rotationBlock)
-    }
-    
-    public func stopRotatingTicker() {
-        tickerLayer.removeAnimationForKey(GNTickerButton.kTickerRotationAnimationKey)
-    }
-    
-    public func clearRingProgress() {
-        CATransaction.begin()
-        CATransaction.setCompletionBlock() {
-            CATransaction.begin()
-            self.ringLayer.strokeStart = 0
-            self.ringLayer.strokeEnd = 0
-            CATransaction.commit()
-        }
-        ringLayer.strokeStart = 1
-        CATransaction.commit()
-    }
-    
-    //MARK: Private
-    
-    private func _rotateTickerWithDuration(duration:CFTimeInterval, rotations repeatCount:Int = 1, shouldSetDesiredRotationCount:Bool = true, rotationBlock: (Void -> Void)?) {
-        tickerIsSpinning = true
-        
-        if (shouldSetDesiredRotationCount) {
-            desiredRotations = repeatCount
-        }
-        
-        let rotationAnimation = CABasicAnimation(keyPath: GNTickerButton.kTickerRotationAnimationKey)
-        rotationAnimation.duration = duration
-        rotationAnimation.fromValue = 0
-        rotationAnimation.toValue = 2*M_PI
-        rotationAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        
-        var repeats = repeatCount
-        
-        CATransaction.begin()
-        CATransaction.setCompletionBlock() {
-            dispatch_async(dispatch_get_main_queue()) {
-                self.updateRingProgress(repeatCount, animated: true)
-                if (rotationBlock != nil) {
-                    rotationBlock!()
-                } else {
-                    self.delegate?.tickerButtonTickerRotated(tickerButton: self)
-                }
-                if (repeats > 0) {
-                    self.rotateTickerWithDuration(duration, rotations: --repeats, rotationBlock: rotationBlock)
-                } else {
-                    self.desiredRotations = nil
-                    self.tickerIsSpinning = false
-                }
-            }
-        }
-        
-        tickerLayer.addAnimation(rotationAnimation, forKey: GNTickerButton.kTickerRotationAnimationKey)
-        CATransaction.commit()
-    }
-    
-    private func updateRingProgress(rotationsLeft:Int, animated:Bool) {
-        var strokeEnd = 0 as CGFloat
-        if (desiredRotations != nil) {
-            strokeEnd = CGFloat((desiredRotations! - rotationsLeft)) / CGFloat(desiredRotations!)
-        }
-        
-        let fillAnimation = CABasicAnimation(keyPath: GNTickerButton.kRingProgressAnimationKey)
-        fillAnimation.duration = animated ? GNTickerButton.kRingProgressAnimationDuration : 0
-        fillAnimation.fromValue = ringLayer.strokeEnd
-        fillAnimation.toValue = strokeEnd
-        fillAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        
-        ringLayer.strokeEnd = strokeEnd
-        
-        CATransaction.begin()
-        ringLayer.addAnimation(fillAnimation, forKey: GNTickerButton.kRingProgressAnimationKey)
-        CATransaction.commit()
-    }
-    
-    //MARK: - Drawing
-    
-    override public func drawRect(rect: CGRect) {
-        super.drawRect(rect)
-        
-        func addCircleInContext(context:CGContextRef, centerX:CGFloat, centerY:CGFloat, radius:CGFloat) {
-            CGContextAddArc(context, centerX, centerY, radius, CGFloat(0), CGFloat(2*M_PI), 0)
-        }
-        
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
-        
-        let color = isPressed ? fillColor.colorWithAlphaComponent(0.5) : fillColor
-        CGContextSetFillColorWithColor(context, color.CGColor)
-        
-        CGContextSetStrokeColorWithColor(context, ringColor.CGColor)
-        
-        let outerRadius = outerRadiusInRect(rect)
-        let innerRadius = outerRadius - GNTickerButton.kOutterInnerRingSpacing
-        
-        let centerX = CGRectGetMidX(rect)
-        let centerY = CGRectGetMidY(rect)
-        
-        // Inner Circle
-        addCircleInContext(context, centerX: centerX, centerY: centerY, radius: innerRadius)
-        CGContextFillPath(context)
-        
-        // Inner Ring
-        CGContextSetLineWidth(context, GNTickerButton.kInnerRingLineWidth)
-        addCircleInContext(context, centerX: centerX, centerY: centerY, radius: innerRadius)
-        CGContextStrokePath(context)
-    }
-    
-    override public func drawLayer(layer: CALayer, inContext ctx: CGContext) {
-        super.drawLayer(layer, inContext: ctx)
-        setUpTicker()
-        setUpRing()
-    }
-    
-    //MARK - Helpers
-    
-    private func outerRadiusInRect(rect:CGRect) -> CGFloat {
-        return rect.width/2 - 2
-    }
-    
+    // Inner Ring
+    context.setLineWidth(GNTickerButton.kInnerRingLineWidth)
+    addCircleInContext(context, centerX: centerX, centerY: centerY, radius: innerRadius)
+    context.strokePath()
+  }
+  
+  override open func draw(_ layer: CALayer, in ctx: CGContext) {
+    super.draw(layer, in: ctx)
+    setUpTicker()
+    setUpRing()
+  }
+  
+  //MARK - Helpers
+  
+  fileprivate func outerRadiusInRect(_ rect:CGRect) -> CGFloat {
+    return rect.width/2 - 2
+  }
+  
 }
